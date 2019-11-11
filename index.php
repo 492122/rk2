bd08b7563951b90bf20c6839ead9452700843a0e
492122 <56249010+492122@users.noreply.github.com>
2019-11-11 11:17:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
 
+<html> 
+<head>
+<meta charset="UTF-8">;
+<title> RK2 </title>
+<h1> Ширинов Платон </h1>
+<h2> Вариант 2 </h2>
+<h2> Задание 2 </h2>
+<h2> Задание 1 </h2>
+<body>
+<input type="number" name="a" value=100><br>;
+<input type="number" name="b" value=100><br>;
+<input type="number" name="c" value=100><br>;
+
+ <form action=" " method="POST">;
+ </form>
+ <?php
+ (0<>($_POST["a"]))($m = ($_POST["b"])*($_POST["b"])-4*($_POST["a"])($_POST["c"])
+ if ($m>==0)  {
+echo "X1=", (-$_POST["a"] - $m)/2*($_POST["a"]); <br>
+echo "X2=", (-$_POST["a"] + $m)/2*($_POST["a"]); <br>
+ }
+<?
+</body>
+</html>
+<html> 
+<head>
+<body>
+<input type ="number"    name="a1"    min="1900"max="2100" required><br>
+</body>
+<?php
+for ($i=$_POST[a1], $i<2101; $i++) {
+echo $i;
+}
+</body>
+</html>
